c194899fdafc08b767d1b28b1f0a4df4df24157c
mvanhulzen <github@mvanhulzen.nl>
2014-10-22 16:31:04 +0200

    Now with credentials in cookie (no chocolat chip)

--- js/VeldWerkCalls.js
+++ js/VeldWerkCalls.js
@@ -18,20 +18,60 @@ define([
     return declare([], {
         portal:null,
         portalUrl: null,
-
+        credCookieName:"veldwerk_identmanager",
         constructor: function(options){
             portalUrl = document.location.protocol + '//www.arcgis.com';
             //create the portalObject
             portal = new arcgisPortal.Portal(portalUrl);
+            
+            var idJson = cookie(credCookieName);
+
+
+
+            if (idJson)
+            {
+                var idObject = JSON.parse(idJson);
+                esri.id.initialize(idJson);
+
+                var cred = esri.id.findCredential(portalUrl)
+                if (!cred) {
+                    portal.signIn();
+                }
+            }
         },
     
+    
+        getUser: function()
+        {
+            return portal.user;
+        }
     
         signIn: function()
         {
-            return portal.signIn();
+           var def = portal.signIn();
+            def.then(function (result)
+            {
+                
+                syncCredsToCookie();
+                
+            });
+            return def;
 
         },
+        
+        signOut:function()
+        {
+            portal.signOut();
+            esri.id.destroyCredentials();
+            syncCredsToCookie();
+        }
 
+        syncCredsToCookie:function()
+        {
+            var json = JSON.stringify(esri.id.toJson());
+
+            cookie(credCookieName, json, { expires: 5 });
+        }
         
         getWebMapsForUser:function()
         {
