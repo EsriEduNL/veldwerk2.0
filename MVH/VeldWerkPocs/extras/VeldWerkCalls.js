97453aa09443a7c576ce9ad8000f433f159cb52a
mvanhulzen <github@mvanhulzen.nl>
2014-10-22 12:16:44 +0200

    Eerste versie VeldWerkCalls.js

---
+++ MVH/VeldWerkPocs/extras/VeldWerkCalls.js
@@ -0,0 +1,69 @@
+define([
+  "dojo/_base/declare",
+  "dojo/_base/lang",
+
+  "esri/request",
+  "esri/arcgis/Portal",
+  "esri/config"
+], function (
+  declare,
+  lang,
+
+  esriRequest,
+  arcgisPortal,
+  config
+
+) {
+
+    return declare([], {
+        portal:null,
+        portalUrl: null,
+
+        constructor: function(options){
+            portalUrl = document.location.protocol + '//www.arcgis.com';
+            //create the portal
+            portal = new arcgisPortal.Portal(portalUrl);
+        },
+    
+    
+        signIn: function()
+        {
+            return portal.signIn();
+
+        },
+
+        
+        getWebMapsForUser:function()
+        {
+            if(portal.user)
+            {
+                var requestUrl = portalUrl + "/sharing/rest/search";
+                var query = 'owner:'+ portal.user.username + ' AND type:"Web Map"'
+                var itemRequest = esriRequest({
+                    url: requestUrl,
+                    content: { f: "json", q: query,num:100},
+                    handleAs: "json"
+                   
+                });
+                return itemRequest;
+            }
+        }
+        ,
+
+        getHello: function ()
+        {
+            return "Hello world";
+        }
+        ,
+        
+        getSla: function()
+        {
+            return "sladiebla" + portalUrl;
+
+        }
+
+
+    });
+}
+);
+
